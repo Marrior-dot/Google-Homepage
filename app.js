60be792337938662cf362e47287c364487464024
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-19 13:56:53 -0300

    html: retirar o <script> q vem junto com o arquivo exportado do codepen, ainda tem o erro da imagem q aparentemente n existe mais, js: configurar resto da janela de personalização, css: acabar de refatorar um dia...

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
-let Pfixed = document.getElementById("personalizar");
-Pfixed.onclick = function (){
-	let newDiv = document.createElement("Person");	
+let person = document.getElementById('personalizar');
+person.onclick = function(){
+	let personParent = person.parentNode.parentNode;
+	personParent.style.textAlign = 'center';
+	personParent.style.fontFamily = "monospace";
+		personParent.innerHTML ="<h1>Personalizar</h1>"
+			personParent.innerHTML += "<fieldset style = 'width: 60%;border-width: 0.1em;border-radius: 0.5em';><legend><h2>Escolher Tema:</h2></legend><div style = 'background-color: red; border-radius: 50%;width: 7em; height: 7em'></div><div style = 'background-color: blue; border-radius: 50%;width: 0.5em; height: 0.5em'></div>"
+		personParent.innerHTML += "</fieldset>"
 }
 
