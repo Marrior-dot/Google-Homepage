17f35babcf7c1b0a72ab5362447b4fbf3146c9e8
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-20 15:20:25 -0300

    aba de personalizar criada faltando apenas criar as opções de cores dentro da aba

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 let person = document.getElementById('personalizar');
-/*person.onclick = function(){
-	let personParent = person.parentNode.parentNode;
-	personParent.style.textAlign = 'center';
-	personParent.style.fontFamily = "monospace";
-		personParent.innerHTML ="<h1>Personalizar</h1>"
-			personParent.innerHTML += "<fieldset style = 'display: flex;justify-content: space-evenly;width: 60%;border-width: 0.1em;border-radius: 0.5em';><legend><h2>Escolher Tema</h2></i></legend><div style = 'background-color: red; border-radius: 50%;width: 7em; height: 7em'></div><div style = 'background-color: blue; border-radius: 50%;width: 7em; height: 7em'></div>"
-		personParent.innerHTML += "</fieldset>"
-}
-*/
-
 person.onclick = function(){
 	let tab = document.createElement('div');
 	let body = document.getElementById("documentB")
@@ -16,8 +6,22 @@ person.onclick = function(){
 		body.children[i].style.filter = 'blur(4px)'
 	}	
 	tab.className = 'Personalized';
-	tab.innerHTML = "<fieldset style = 'border-radius: 0.5em;border-width: 1px'><h4 style ='text-align: center'> Cor e tema </h4><div style = 'background-color: black; width: 2em; height: 2em; border-radius: 50%'></div>";
+
+	tab.innerHTML = "<fieldset style = 'border-radius: 0.5em;border-width: 1px'> <h2 style ='text-align: center'>Cor e tema<i class='fas fa-adjust'></i></h2><section id = 'PField' > <div onmouseover ='changeBorder()' class = 'Theme' style = 'background-color: black; width: 4em; height: 4em; border-radius: 50%'></div> <div class = 'Theme' onmouseover ='changeBorder()' style = 'background-color: LightBlue; width: 4em; height: 4em; border-radius: 50%'></div></section>";
 	tab.innerHTML += "</fieldset>"
+
 	body.appendChild(tab);
 	person.disabled = true;
+
+	let sectionTheme = document.querySelector('.Theme'); 
+	function changeBorder(){
+		sectionTheme.style.backgroundColor = 'black';	
+	}
+/*	document.getElementsByClassName('Theme').onmouseover = function(){
+	document.getElementsByClassName('Theme').style.borderWidth = '20px';
+	document.getElementsByClassName('Theme').style.borderStyle = 'solid';
+	document.getElementsByClassName('Theme').style.borderColor = 'LightBlue';
+	}
+}
+*/
 }
