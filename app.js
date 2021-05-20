4514485ae015e1ae4ed070c4415d6e3a3159fd2a
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-20 09:44:46 -0300

    novas funcionalidades

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ person.onclick = function(){
 	personParent.style.textAlign = 'center';
 	personParent.style.fontFamily = "monospace";
 		personParent.innerHTML ="<h1>Personalizar</h1>"
-			personParent.innerHTML += "<fieldset style = 'width: 60%;border-width: 0.1em;border-radius: 0.5em';><legend><h2>Escolher Tema:</h2></legend><div style = 'background-color: red; border-radius: 50%;width: 7em; height: 7em'></div><div style = 'background-color: blue; border-radius: 50%;width: 0.5em; height: 0.5em'></div>"
+			personParent.innerHTML += "<fieldset style = 'display: flex;justify-content: space-evenly;width: 60%;border-width: 0.1em;border-radius: 0.5em';><legend><h2>Escolher Tema</h2></i></legend><div style = 'background-color: red; border-radius: 50%;width: 7em; height: 7em'></div><div style = 'background-color: blue; border-radius: 50%;width: 7em; height: 7em'></div>"
 		personParent.innerHTML += "</fieldset>"
 }
 
