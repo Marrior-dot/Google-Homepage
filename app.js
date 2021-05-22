b51d1f82e418ba4a56dac8d4632d0f125b91df62
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-22 15:02:14 -0300

    personalização possui 3 temas e alterna entre esses

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ let listOfButtons = document.getElementsByClassName('borderA');
 let svButton = document.getElementById('checkpoint');
 let main = document.getElementById("documentM");
 let GLetters = document.querySelectorAll('.Tchange');
+console.log(GLetters);
 
 let popUps = {
 	salvar: function buttonSave(){
@@ -27,7 +28,47 @@ let popUps = {
 			for(var j = 0;j < GLetters.length;j = j+1 ){
 			GLetters[j].style.color = 'white';
 		}
+		},
+	BlueTheme: function BlueTheme(){
+		document.getElementById("documentB").style.backgroundColor = 'LightBlue'
+		document.getElementById('mail').style.color = 'white';
+		document.getElementById('img').style.color = 'white';	
+		document.getElementById('user').style.backgroundColor = 'white';
+		document.getElementById('hamb').style.color = 'white';
+		document.querySelector('.foot').style.color = '#2A97BA';
+		document.querySelector('.contact').style.color ='#2A97BA'; 
+		document.querySelector('.contact').children[1].style.color ='#2A97BA';
+		document.querySelector('.contact').children[2].style.color ='#2A97BA'; 
+			for(var j = 0;j < GLetters.length;j = j+1 ){
+			GLetters[j].style.color = "#2A97BA";
 		}
+	},
+	Defaulttheme: function defaultTheme(){
+		document.getElementById("documentB").style.backgroundColor = 'initial';
+		document.getElementById('mail').style.color = 'initial';
+		document.getElementById('img').style.color = 'initial';	
+		document.getElementById('user').style.backgroundColor = 'initial';	
+		document.getElementById('hamb').style.color = 'initial';	
+		document.querySelector('.foot').style.color = 'initial';	
+		document.querySelector('.contact').style.color ='initial';	
+		document.querySelector('.contact').children[1].style.color ='initial';	
+		document.querySelector('.contact').children[2].style.color ='initial';	
+			for(var j = 0;j < GLetters.length;j = j+1 ){
+			//GLetters[j].style.color = 'initial';	
+				if (j === 0 || j ===3){
+					GLetters[j].style.color = '#4285f4';
+				}
+				else if (j===1 || j===5){
+					GLetters[j].style.color ='#ea4335'; 
+				}
+				else if (j===2){
+					GLetters[j].style.color = '#fbbc05';
+				}
+				else{
+					GLetters[j].style.color = '#34a853';
+				}
+		}
+	}
 	}
 person.onclick = function(){
 	main.previousElementSibling.style.filter = 'blur(6px)';
@@ -42,4 +83,6 @@ person.onclick = function(){
 }
 
 listOfButtons[0].onclick = popUps.BlackTheme;
+listOfButtons[1].onclick = popUps.BlueTheme;
+listOfButtons[2].onclick = popUps.Defaulttheme;
 svButton.onclick = popUps.salvar; 
